501eaa437230719f930775cba2ed71ae08a567c4
Ilvars <iplavins@gmail.com>
2025-09-25 07:19:59 +0000

    Apraksts par izmaiņām

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
 <title>Datu portals</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
-<link rel="stylesheet" href="./assets/css/index.css"/>
+<link rel="stylesheet" href="assets/css/index.css"/>
 
 </head>
 <body>
