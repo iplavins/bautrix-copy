30f820bc30bce481731d458de4be73570fc1e48e
Ilvars <iplavins@gmail.com>
2025-09-25 08:06:53 +0000

    Apraksts par izmaiņām

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>Datu portals</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="/assets/css/index.css">
 
 </head>
 <body>
